e5472fa0158316d03f9f00a97ddb9217a198b44e
G Johansson <goran.johansson@shiftit.se>
2022-02-19 15:05:12 +0100

    Rewrite pysensibo

---
+++ pysensibo/exceptions.py
@@ -0,0 +1,17 @@
+"""Exceptions for Sensibo."""
+
+from typing import Any
+
+class SensiboError(Exception):
+    """Error from Sensibo api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+class AuthenticationError(Exception):
+    """Authentication issue from Sensibo api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
