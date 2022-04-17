710bbc4dd7e366f9b6892535565ccf9d1b9b94bd
G Johansson <goran.johansson@shiftit.se>
2022-04-17 18:26:26 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ from setuptools import setup, find_packages
 
 print(find_packages())
 setup(
-    name='pysensibo',
-    version='1.0.10',
-    description='asyncio-friendly python API for Sensibo',
-    long_description='asyncio-friendly python API for Sensibo'
-                     '(https://sensibo.com). Requires Python 3.4+',
-    url='https://github.com/andrey-git/pysensibo',
-    license='MIT',
+    name="pysensibo",
+    version="1.0.11",
+    description="asyncio-friendly python API for Sensibo",
+    long_description="asyncio-friendly python API for Sensibo"
+    "(https://sensibo.com). Requires Python 3.4+",
+    url="https://github.com/andrey-git/pysensibo",
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='Sensibo',
-    install_requires=['aiohttp'],
+    keywords="Sensibo",
+    install_requires=["aiohttp"],
     zip_safe=True,
-    author='andrey-git',
-    author_email='andrey-git@users.noreply.github.com',
-    packages=find_packages()
+    author="andrey-git",
+    author_email="andrey-git@users.noreply.github.com",
+    packages=find_packages(),
 )
