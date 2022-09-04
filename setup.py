bd4bc16a93c119bea04b6f7bc73e9b927c549107
G Johansson <goran.johansson@shiftit.se>
2022-09-04 16:56:39 +0200

    v1.0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     name="pysensibo",
-    version="1.0.18",
+    version="1.0.19",
     description="asyncio-friendly python API for Sensibo",
     long_description="asyncio-friendly python API for Sensibo"
     "(https://sensibo.com). Requires Python 3.4+",
